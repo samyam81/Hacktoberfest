fd1b11eec147a68d1b32f43479a98cf8bef7ec02
Samyam Subedi <samyam081@gmail.com>
2024-06-19 13:49:40 +0545

    Update fibonacci.go

--- Languages/Go/fibonacci.go
+++ Languages/Go/fibonacci.go
@@ -1,27 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-
 	var n int
 	fmt.Print("Enter the number of digits to print: ")
 	fmt.Scanf("%d", &n)
 
-	var a, b, c int64
-    a = 0;
-    b = 1;
-	n -= 2;
-
-	fmt.Printf("%d %d ",a ,b)
-	for n > 0 {
-		n--
-
-		c = a + b
-		fmt.Printf("%d ", c)
-		a = b
-		b = c
+	if n <= 0 {
+		fmt.Println("Invalid input. Please enter a positive number.")
+		return
 	}
-	fmt.Print("\r\n")
 
+	// Edge case: Print first Fibonacci number if n is 1
+	if n >= 1 {
+		fmt.Printf("0 ")
+	}
+	if n >= 2 {
+		fmt.Printf("1 ")
+	}
+
+	// Array to store Fibonacci numbers
+	fib := make([]int64, n)
+	fib[0] = 0
+	fib[1] = 1
+
+	for i := 2; i < n; i++ {
+		fib[i] = fib[i-1] + fib[i-2]
+	}
+
+	// Print the Fibonacci sequence
+	for i := 2; i < n; i++ {
+		fmt.Printf("%d ", fib[i])
+	}
+	fmt.Println()
 }
