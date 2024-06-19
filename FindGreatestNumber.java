4af0d9d3e51998868393957fd6990d8f75ff53e9
Samyam Subedi <samyam081@gmail.com>
2024-06-19 13:46:43 +0545

    Update FindGreatestNumber.java

--- FindGreatestNumber.java
+++ FindGreatestNumber.java
@@ -1,35 +1,40 @@
 import java.util.Scanner;
+
 public class FindGreatestNumber {
-    int num1,num2,num3;
-    public void setvalue(){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the first number:");
-        num1 = sc.nextInt();
-        System.out.print("Enter the second number:");
-        num2 = sc.nextInt();
-        System.out.print("Enter the third number:");
-        num3 = sc.nextInt();
-        sc.close();
-    }
-    public void calcGreatest(){
-        if(num1 > num2 && num1 > num3)
-        {
-            System.out.println("Greatest number is:"+num1);
+    private int num1, num2, num3;
+
+    public void setInputValues() {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter the first number: ");
+            num1 = sc.nextInt();
+            System.out.print("Enter the second number: ");
+            num2 = sc.nextInt();
+            System.out.print("Enter the third number: ");
+            num3 = sc.nextInt();
+        } catch (Exception e) {
+            System.out.println("Invalid input. Please enter valid integers.");
+            System.exit(1); // Exit the program on error
         }
-        else if(num2 > num3)
-        {
-            System.out.println("Greatest number is:"+num2);
-        }
-        else
-        {
-            System.out.println("Greatest number is:"+num3);
-        }
-    }
-    public static void main(String[] args) {
-        System.out.println("Program to Find Greatest Number");
-        FindGreatestNumber g = new FindGreatestNumber();
-        g.setvalue();
-        g.calcGreatest();
     }
 
+    public void findGreatestNumber() {
+        int greatest = num1; // Assume num1 is the greatest initially
+
+        if (num2 > greatest) {
+            greatest = num2;
+        }
+        if (num3 > greatest) {
+            greatest = num3;
+        }
+
+        System.out.println("Greatest number is: " + greatest);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Program to Find Greatest Number");
+
+        FindGreatestNumber finder = new FindGreatestNumber();
+        finder.setInputValues();
+        finder.findGreatestNumber();
+    }
 }
